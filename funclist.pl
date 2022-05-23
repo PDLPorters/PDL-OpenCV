08298f306f234b5669abb27dffa700d14c2dfad8
Ed J <mohawk2@users.noreply.github.com>
2022-05-24 00:16:06 +0100

    get top-level genwrap data from subdirs

--- funclist.pl
+++ funclist.pl
@@ -1,11 +1,6 @@
 (
 ['','normalize',"",0,'void',['Mat','mw'],['Mat','out'],['int','start'],['int','end'],['int','type']],
 ['','minMaxIdx',"",0,'void',['Mat','mw'],["double *","mymin"],["double *","mymax"]],
-['','imshow',"",0,'void',['const char *','name'],['Mat','mw']],
-['','waitKey',"",0,'int',['int','delay']],
-['','namedWindow',"",0,'void',['const char *','winname'],['int','flags']],
-['','selectROI',"",0,'Rect',['const char *','windowName'],['Mat','img'],['char','showCrosshair',"1"],["char","fromCenter","0"]],
-['','destroyWindow',"",0,'void',['const char *','winname']],
 ['','cvtColor',"",0,'void',["Mat","src","",[]],["Mat","dst","",["/O"]],["int","code","",[]],["int","dstCn","0",[]]],
 ['VideoWriter','fourcc',"",0,'int',['char','c1'],['char','c2'],['char','c3'],['char','c4']],
 ['VideoWriter','open',"",1,'char',['const char *','name'],['int','fourcc'],['double','fps'],['Size','size'],['char','iscolor']],
@@ -16,7 +11,7 @@
 ['Mat','channels',"",1,'int'],
 ['Mat','ptr',"",1,'void *'],
 ['Mat','convertTo',"",1,'void',['Mat','out'],['int','rtype'],['double','alpha'],['double','beta']],
-['','rectangle',"",0,"void",["Mat","img"],["Rect","rec"],["Scalar","color"],["int","thickness"],["int","lineType"],["int","shift"]],
-['Tracker','init',"",1,'void',['Mat','mw'],['Rect','boundingBox']],
-['Tracker','update',"",1,'char',['Mat','mw'],['Rect','boundingBox']],
+do './Imgproc/funclist.pl',
+do './Tracker/funclist.pl',
+do './Highgui/funclist.pl',
 );
