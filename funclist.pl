270dbde0392124869e0ffb69ac49962e0d14c75e
Ed J <mohawk2@users.noreply.github.com>
2022-05-24 01:21:01 +0100

    zap wrapping of channels method

--- funclist.pl
+++ funclist.pl
@@ -2,7 +2,6 @@
 ['','normalize',"",0,'void',['Mat','mw'],['Mat','out'],['int','start'],['int','end'],['int','type']],
 ['','minMaxIdx',"",0,'void',['Mat','mw'],["double *","mymin"],["double *","mymax"]],
 ['','cvtColor',"",0,'void',["Mat","src","",[]],["Mat","dst","",["/O"]],["int","code","",[]],["int","dstCn","0",[]]],
-['Mat','channels',"",1,'int'],
 ['Mat','convertTo',"",1,'void',['Mat','out'],['int','rtype'],['double','alpha'],['double','beta']],
 do './Videoio/funclist.pl',
 do './Imgproc/funclist.pl',
