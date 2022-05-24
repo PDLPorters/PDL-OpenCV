2ac83bff46ce2ca9db37fa76ac575243b503595a
Ed J <mohawk2@users.noreply.github.com>
2022-05-24 04:18:21 +0100

    locate cvtColor correctly in Imgproc

--- Imgproc/funclist.pl
+++ Imgproc/funclist.pl
@@ -1,3 +1,4 @@
 (
   ['','rectangle',"Draw rectangle",0,"void",["Mat","img"],["Rect","rec"],["Scalar","color"],["int","thickness"],["int","lineType"],["int","shift"]],
+  ['','cvtColor',"convert src to dest with given colour space",0,'void',["Mat","src","",[]],["Mat","dst","",["/O"]],["int","code","",[]],["int","dstCn","0",[]]],
 );
