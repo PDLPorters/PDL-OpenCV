b632f23548809317f7cab6edf568eaabb6f0ba53
Ed J <mohawk2@users.noreply.github.com>
2023-01-15 15:43:05 +0000

    zap unused, non-Python-wrapped func

--- funclist.pl
+++ funclist.pl
@@ -1,5 +1,4 @@
 (
 ['','normalize',"",0,'void',['Mat','mw'],['Mat','out'],['int','start'],['int','end'],['int','type']],
 ['','minMaxIdx',"returns minimum and maximum values of Mat-wrapped ndarray",0,'void',['Mat','mw'],["double *","min","",["/O"]],["double *","max","",["/O"]]],
-['Mat','convertTo',"",1,'void',['Mat','out'],['int','rtype'],['double','alpha'],['double','beta']],
 );
