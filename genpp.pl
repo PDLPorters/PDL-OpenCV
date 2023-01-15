d5440cef00deb88f85261a384aa65e9706ac94b4
Ed J <mohawk2@users.noreply.github.com>
2023-01-15 23:37:45 +0000

    better var-name

--- genpp.pl
+++ genpp.pl
@@ -100,7 +100,7 @@ sub destroy_code {
 sub genpp {
     my ($class,$func,$doc,$ismethod,$ret,@params) = @_;
     die "No class given for method='$ismethod'" if !$class and $ismethod;
-    $_ = '' for my ($callprefix, $compmode);
+    $_ = '' for my ($retcapture, $compmode);
     my %hash = (GenericTypes=>$T, NoPthread=>1, HandleBad=>0, Doc=>"=for ref\n\n$doc");
     $hash{PMFunc} = '' if $ismethod;
     my $pcount = 1;
@@ -162,7 +162,7 @@ EOF
       push @defaults, "\$$var = $default if !defined \$$var;" if length $default;
     }
     push @pp_input, map $_->[1], @otherpars;
-    $callprefix = ($ret =~ /^[A-Z]/ ? 'res' : '$res()').' = ', pop @c_input if $ret ne 'void';
+    $retcapture = ($ret =~ /^[A-Z]/ ? 'res' : '$res()'), pop @c_input if $ret ne 'void';
     %hash = (%hash,
       Pars => join('; ', @pars), OtherPars => join('; ', map $_->[0], @otherpars),
       PMCode => <<EOF,
@@ -183,19 +183,19 @@ EOF
         (map "PDL_RETERROR(PDL_err, PDL->make_physical($_->[1]));\n", grep ref, @c_input),
         (map $_->[1] ? "\$COMP($_->[0]) = $_->[5];\n" : "@$_[2,0]_LOCAL = ".$_->[6]->(1).";\n", @pdl_inits),
         (!@pdl_inits ? () : qq{if (@{[join ' || ', map "!".($_->[1]?"\$COMP($_->[0])":"$_->[0]_LOCAL"), @pdl_inits]}) {\n$destroy_in$destroy_out\$CROAK("Error during initialisation");\n}\n}),
-        ($callprefix && '$COMP(res) = ').$cfunc."(".join(',', map ref()?"$_->[0](($_->[2]*)($_->[1]->data))[0]":$var2usecomp{$_}?"\$COMP($_)":$_.'_LOCAL', @c_input).");\n",
+        ($retcapture && '$COMP(res) = ').$cfunc."(".join(',', map ref()?"$_->[0](($_->[2]*)($_->[1]->data))[0]":$var2usecomp{$_}?"\$COMP($_)":$_.'_LOCAL', @c_input).");\n",
         $destroy_in;
       $hash{CompFreeCodeComp} = $destroy_out;
       my @map_tuples = map [$_->[1], $var2count{$_->[1]}, map $_->(1), @$_[2,3]], grep $var2count{$_->[1]}, @outputs;
       $hash{RedoDimsCode} = join '', map "$_->[2];\n", @map_tuples;
       $hash{Code} = join '', map "$_->[3];\n", @map_tuples;
-      $hash{Code} .= $callprefix.'$COMP(res);'."\n" if $callprefix;
+      $hash{Code} .= "$retcapture = \$COMP(res);\n" if $retcapture;
     } else {
       my @map_tuples = map [$_->[1], $var2count{$_->[1]}, map $_->(0), @$_[2,3]], grep $var2count{$_->[1]}, @outputs;
       $hash{Code} = join '',
         (map "@$_[2,0] = ".$_->[6]->(0).";\n", @pdl_inits),
         (!@pdl_inits ? () : qq{if (@{[join ' || ', map "!$_->[0]", @pdl_inits]}) {\n$destroy_in$destroy_out\$CROAK("Error during initialisation");\n}\n}),
-        $callprefix.$cfunc."(".join(',', map ref()?"$_->[0]\$$_->[1]()":$var2usecomp{$_}?"\$COMP($_)":$_, @c_input).");\n",
+        ($retcapture && "$retcapture = ")."$cfunc(".join(',', map ref()?"$_->[0]\$$_->[1]()":$var2usecomp{$_}?"\$COMP($_)":$_, @c_input).");\n",
         (map "$_->[3];\n", @map_tuples),
         $destroy_in, $destroy_out;
     }
