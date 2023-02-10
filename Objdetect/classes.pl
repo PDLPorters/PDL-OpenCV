3b0f13b7801baf52c584e3e202729bf5db45b844
Ed J <mohawk2@users.noreply.github.com>
2023-02-10 19:53:56 +0000

    objdetect for QRCodeDetector

---
+++ Objdetect/classes.pl
@@ -0,0 +1,7 @@
+(
+['QRCodeDetector','@brief Groups the object candidate rectangles.
+    @param rectList  Input/output vector of rectangles. Output vector includes retained and grouped rectangles. (The Python list is not modified in place.)
+    @param weights Input/output vector of weights of rectangles. Output vector includes weights of retained and grouped rectangles. (The Python list is not modified in place.)
+    @param groupThreshold Minimum possible number of rectangles minus 1. The threshold is used in a group of rectangles to retain it.
+    @param eps Relative difference between sides of the rectangles to merge them into a group.'],
+);
---
+++ Objdetect/funclist.pl
@@ -0,0 +1,7 @@
+(
+['QRCodeDetector','detectAndDecodeCurved','@brief Both detects and decodes QR code on a curved surface
+
+     @param img grayscale or color (BGR) image containing QR code.
+     @param points optional output array of vertices of the found QR code quadrangle. Will be empty if not found.
+     @param straight_qrcode The optional output image containing rectified and binarized QR code',1,'string',['Mat','img','',[]],['Mat','points','Mat()',['/O']],['Mat','straight_qrcode','Mat()',['/O']]],
+);
