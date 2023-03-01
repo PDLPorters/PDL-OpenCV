82c4d00223387f4b08d5aba4c3c5a5dd91694265
Ed J <mohawk2@users.noreply.github.com>
2023-03-01 04:26:07 +0000

    wrap groupRectangles

--- Objdetect/funclist.pl
+++ Objdetect/funclist.pl
@@ -58,4 +58,5 @@
     @param decoded_info UTF8-encoded output vector of string or empty vector of string if the codes cannot be decoded.
     @param points optional output vector of vertices of the found QR code quadrangles. Will be empty if not found.
     @param straight_qrcode The optional output vector of images containing rectified and binarized QR codes',1,'bool',['Mat','img','',[]],['vector_string','decoded_info','',['/O','/Ref']],['Mat','points','Mat()',['/O']],['vector_Mat','straight_qrcode','vector_Mat()',['/O']]],
+['','groupRectangles','@overload',0,'void',['vector_Rect','rectList','',['/IO','/Ref']],['vector_int','weights','',['/O','/Ref']],['int','groupThreshold','',[]],['double','eps','0.2',[]]],
 );
