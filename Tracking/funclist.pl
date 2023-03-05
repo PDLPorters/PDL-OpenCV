13e9c5cd1cf126a414a1026498215b6bf0854ca0
Ed J <mohawk2@users.noreply.github.com>
2023-03-05 03:24:06 +0000

    get Tracker methods from hdr_parser

--- Tracking/funclist.pl
+++ Tracking/funclist.pl
@@ -1,4 +1,13 @@
 (
-  ['Tracker','init',"Initialize OpenCV tracker",1,'void',['Mat','mw'],['Rect','boundingBox']],
-  ['Tracker','update',"Update tracker with new image",1,'int',['Mat','mw'],['Rect','boundingBox',"",['/O']]],
+['Tracker','init','@brief Initialize the tracker with a known bounding box that surrounded the target
+    @param image The initial frame
+    @param boundingBox The initial bounding box',1,'void',['Mat','image','',[]],['Rect','boundingBox','',['/C','/Ref']]],
+['Tracker','update','@brief Update the tracker, find the new most likely bounding box for the target
+    @param image The current frame
+    @param boundingBox The bounding box that represent the new target location, if true was returned, not
+    modified otherwise
+
+    @return True means that target was located and false means that tracker cannot locate target in
+    current frame. Note, that latter *does not* imply that tracker has failed, maybe target is indeed
+    missing from the frame (say, out of sight)',1,'bool',['Mat','image','',[]],['Rect','boundingBox','',['/O','/Ref']]],
 );
